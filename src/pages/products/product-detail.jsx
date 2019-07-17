68474ab8b5a80b3ad732a6d2816790e5ab8bc22a
Moc Huong Garden <ngannguyen977@gmail.com>
2019-07-17 14:44:19 +0700

    1707

--- src/pages/products/product-detail.jsx
+++ src/pages/products/product-detail.jsx
@@ -6,57 +6,25 @@ class ProductDetail extends Component {
     
     render() {
      let { items } = this.props
-
-            //    return (
-            //     <div className="container">
-            //         <div className="col-md-8">
-            //             <div className="product-img">
-            //                 <img src={items.imageUrl} alt=""/>
-            //             </div>
-            //             <div className="product-des">
-            //                 <div className="product-tittle">
-            //                     <h2>{items.name}</h2>
-            //                 </div>
-            //                 <div className="product-description">
-            //                     <p>
-            //                         {items.description}
-            //                     </p>
-            //                 </div>
-            //             </div>
-            //         </div>
-            //     </div>
-            //    )
-        const items = this.props.?(
-            <div>
-                <section className="profil  py-5 ">
-                    <div className="container">
-                        <div className="col-md-8">
-                            <div className="product-img">
-                                <img src={items.imageUrl} alt="" />
+               return (
+                <div className="container">
+                    <div className="col-md-8">
+                        <div className="product-img">
+                            <img className="img-responsive" src={items.imageUrl} alt=""/>
+                        </div>
+                        <div className="product-des">
+                            <div className="product-tittle">
+                                <h2>{items.name}</h2>
                             </div>
-                            <div className="product-des">
-                                <div className="product-tittle">
-                                    <h2>{items.name}</h2>
-                                </div>
-                                <div className="product-description">
-                                    <p>
-                                        {items.description}
-                                    </p>
-                                </div>
+                            <div className="product-description">
+                                <p>
+                                    {items.description}
+                                </p>
                             </div>
                         </div>
                     </div>
-                </section>
-            </div>
-        ) : (
-            <div classname="center">
-                Loading...
-            </div>
-        )
-        return (
-           {items}
-        )
-        
+                </div>
+               )
     }
 }
 const mapStateToProps = (state, ownProps) => {
